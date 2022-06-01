da362ca76d28468887ca7a7eb32fa560e14414a5
Federico Blengio <fedeeblengio@gmail.com>
2022-06-01 18:57:09 -0300

    buscador y dividir la pantalla

--- src/components/listarUsuariosComponent.vue
+++ src/components/listarUsuariosComponent.vue
@@ -1,4 +1,4 @@
-<template>
+ <template>
   <div>
     <div class="contenedor_menu">
       <h2>Listado de Personas</h2>
@@ -9,11 +9,15 @@
         </router-link>
       </button>
     </div>
-
-    <div class="menu_buscar">
-      <input placeholder="Buscar..." />
+<div>
+ <div class="menu_buscar">
+      <input placeholder="Buscar..." id='filtro'   v-on:keyup="filtrarPorNombre"/>
       <button><i class="fas fa-search"></i></button>
     </div>
+   
+</div>
+   
+    <div class="contenedorGeneral">
     <div class="contenedor_table">
       <table class="table">
         <thead>
@@ -21,47 +25,44 @@
             <th scope="col">Usuario</th>
             <th scope="col">Nombre</th>
             <th scope="col">Rol</th>
-            <th scope="col">Accion</th>
+           
           </tr>
         </thead>
         <tbody>
-          <tr v-for="todo in todosUsuarios" :key="todo.id">
+          <tr v-for="todo in todosUsuarios" :key="todo.id" v-on:click="buscarUser(todo.username)">
             <td>{{ todo.username }}</td>
             <td>{{ todo.nombre }}</td>
             <td>{{ todo.ou }}</td>
-            <td>
-              <router-link
-                :to="{
-                  name: 'listar-usuario-modificar',
-                  params: { user: todo.username },
-                }"
-              >
-                <i
-                  class="far fa-pen"
-                  style="font-size: 20px; margin-left: 20px; color: blue"
-                ></i>
-              </router-link>
-              <router-link
-                :to="{
-                  name: 'listar-usuario-eliminar',
-                  params: { user: todo.username },
-                }"
-              >
-                <i
-                  class="far fa-user-times"
-                  style="font-size: 20px; margin-left: 20px; color: red"
-                ></i>
-              </router-link>
-            </td>
+         
           </tr>
         </tbody>
       </table>
     </div>
+    <div class="contenedorDerechoPersona">
+      <div class="imgContDer">
+        <img src="https://sportshub.cbsistatic.com/i/2022/02/26/a7550e46-0c91-42a2-92c7-ee68ce8b2976/bleach-art.jpg" alt="">
+      </div>
+      <div class="DerTexl">
+        <div><span>Cedula:</span> <span>4989522</span></div>
+        <div><span>Nombre:</span> <span>Ichigo</span></div>
+        <div><span>Cargo:</span> <span>Shinigami</span></div>
+        <div><span>Correo:</span> <span>fedeblengoo@gmail.com</span></div>
+      </div>
+      <div class="DerTexl">
+      <div><i class="fas fa-pencil-alt"></i> Modificar Usuario</div> 
+        <div><i class="fas fa-trash-alt"></i>Eliminar Usuario</div>
+        
+
+      </div>
+    </div>
+    </div>
+    
   </div>
 </template>
 <script>
 import { Global } from "../Global";
 import axios from "axios";
+import $ from "jquery";
 export default {
   name: "listarUsuarios",
   data() {
@@ -96,6 +97,27 @@ export default {
             message: "Error inesperado al cargar ",
           });
         });
+    },
+    buscarUser(username){
+    alert(username)
+    },
+    filtrarPorNombre() {
+      var input = document.getElementById("filtro").value.toLowerCase();
+      var listaUser = [];
+
+    if (input == "") {
+        this.loading = true;
+        this.getTodos();
+      }
+      this.todosUsuarios.forEach(function (users) {
+        if (users.nombre.toLowerCase().indexOf(input) !== -1)
+          listaUser.push(users);
+      });
+
+      this.todosUsuarios = listaUser;
+    },
+    comprobarArrayVacio(array) {
+      return $.isEmptyObject(array);
     },
   },
 };
