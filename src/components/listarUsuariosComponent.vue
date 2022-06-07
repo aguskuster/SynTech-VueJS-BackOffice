534bdd80c6ec09ab99d56a2fb18eaeb902d5a4e2
Federico Blengio <fedeeblengio@gmail.com>
2022-06-06 22:18:53 -0300

    mañana sigo

--- src/components/listarUsuariosComponent.vue
+++ src/components/listarUsuariosComponent.vue
@@ -2,7 +2,6 @@
   <div>
     <div class="contenedor_menu">
       <h2>Listado de Personas</h2>
-
       <button
         class="btn btn-primary"
         data-bs-toggle="modal"
@@ -12,66 +11,81 @@
       </button>
     </div>
 
-    <div class="menu_buscar">
-      <input
-        placeholder="Buscar..."
-        id="filtro"
-        v-on:keyup="filtrarPorNombre"
-      />
-
-      <select name="" v-on:change="filtrarPorRol()" v-model="selectedRol">
-        <option value="Alumno">Alumnos</option>
-        <option value="Profesor">Profesores</option>
-        <option value="Adscripto">Adscripto</option>
-        <option value="Director">Director</option>
-        <option value="Subdirector">Subdirector</option>
-        <option value="Bedelias">Bedelias</option>
-      </select>
-    </div>
     <div class="contenedorGeneral">
-      <div class="contenedor_table" style="width:70%">
-        <table class="table" >
-          <thead>
-            <tr>
-              <th scope="col">Usuario</th>
-              <th scope="col">Nombre</th>
-              <th scope="col">Rol</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="todo in filtrarPorRol()"
-              :key="todo.id"
-              v-on:click="buscarUser(todo.id)"
-            >
-              <td>{{ todo.id }}</td>
-              <td>{{ todo.nombre }}</td>
-              <td>{{ todo.ou }}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div
+        class="contenedorIzquierdo"
+        style="width: 70%; background-color: transparent"
+      >
+        <div class="menu_buscar" style="margin-bottom: 30px; display: block">
+          <input
+            placeholder="Buscar..."
+            id="filtro"
+            v-on:keyup="filtrarPorNombre"
+          />
+
+          <select name="" v-on:change="filtrarPorRol()" v-model="selectedRol">
+            <option value="Alumno">Alumnos</option>
+            <option value="Profesor">Profesores</option>
+            <option value="Adscripto">Adscripto</option>
+            <option value="Director">Director</option>
+            <option value="Subdirector">Subdirector</option>
+            <option value="Bedelias">Bedelias</option>
+          </select>
+        </div>
+
+        <div class="contenedor_table">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Usuario</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Rol</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="todo in filtrarPorRol()"
+                :key="todo.id"
+                v-on:click="buscarUser(todo.id)"
+              >
+                <td>{{ todo.id }}</td>
+                <td>{{ todo.nombre }}</td>
+                <td>{{ todo.ou }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div class="contenedorDerechoPersona" v-if="showProfile">
-        <div class="imgContDer">
-          <img :src="returnImgProfile(userInfo.profile_img)" />
-        </div>
-        <div class="DerTexl">
-          <div>
-            <span>Cedula:</span> <span>{{ userInfo.user.id }}</span>
+
+      <div
+        class="contenedorDerechoPersona"
+        v-if="showProfile"
+        style="width: 30%; margin-top: 83px; background-color: transparent"
+      >
+        <div class="infoUser">
+          <div class="imgContDer">
+            <center>
+              <img :src="returnImgProfile(userInfo.profile_img)" />
+            </center>
           </div>
-          <div>
-            <span>Nombre:</span> <span>{{ userInfo.user.nombre }}</span>
+          <div class="DerTexl">
+            <div>
+              <span>Cedula:</span> <span>{{ userInfo.user.id }}</span>
+            </div>
+            <div>
+              <span>Nombre:</span> <span>{{ userInfo.user.nombre }}</span>
+            </div>
+            <div>
+              <span>Cargo:</span> <span>{{ userInfo.user.ou }}</span>
+            </div>
+            <div>
+              <span>Correo:</span> <span>{{ userInfo.user.email }}</span>
+            </div>
           </div>
-          <div>
-            <span>Cargo:</span> <span>{{ userInfo.user.ou }}</span>
+          <div class="DerTexl">
+            <div><i class="fas fa-pencil-alt"></i> Modificar Usuario</div>
+            <div><i class="fas fa-trash-alt"></i>Eliminar Usuario</div>
           </div>
-          <div>
-            <span>Correo:</span> <span>{{ userInfo.user.email }}</span>
-          </div>
-        </div>
-        <div class="DerTexl">
-          <div><i class="fas fa-pencil-alt"></i> Modificar Usuario</div>
-          <div><i class="fas fa-trash-alt"></i>Eliminar Usuario</div>
         </div>
       </div>
     </div>
@@ -119,11 +133,11 @@
 import { Global } from "../Global";
 import axios from "axios";
 import $ from "jquery";
-import agregarUsuarioComponent from "./agregarUsuarioComponent"
+import agregarUsuarioComponent from "./agregarUsuarioComponent";
 export default {
   name: "listarUsuarios",
   components: {
-agregarUsuarioComponent
+    agregarUsuarioComponent,
   },
   data() {
     return {
@@ -132,7 +146,6 @@ agregarUsuarioComponent
       showProfile: false,
       selectedRol: "",
     };
-  
   },
 
   mounted() {
