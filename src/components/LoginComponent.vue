0fa4d1abb678b9eaa6b74563cbff22a9d227349d
Federico Blengio <fedeeblengio@gmail.com>
2022-06-22 20:36:40 -0300

    Login Style test

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -10,29 +10,30 @@
             <p class="letraslogin">
               Documento:
               <input
-                type="text"
-                name="username"
-                placeholder="Documento"
-                class="form-control"
-                v-model="contacto.username"
+                  type="text"
+                  name="username"
+                  placeholder="Documento"
+                  class="form-control"
+                  v-model="contacto.username"
               />
             </p>
             <p class="letraslogin">
               Contraseña:
               <input
-                type="password"
-                name="password"
-                placeholder="Contraseña"
-                class="form-control"
-                v-model="contacto.password"
+                  type="password"
+                  name="password"
+                  placeholder="Contraseña"
+                  class="form-control"
+                  v-model="contacto.password"
               />
             </p>
-           <input
-          type="submit"
-          value="Login"
-          title="Enviar"
-          class="btn"
-        />
+            <input
+                type="submit"
+                value="Login"
+                title="Enviar"
+                class="btn"
+                style="border-bottom: 1px solid white; color:white;"
+            />
           </form>
         </div>
       </div>
@@ -41,8 +42,9 @@
 </template>
 
 <script>
-import { Global } from "../Global";
+import {Global} from "../Global";
 import axios from "axios";
+
 export default {
   name: "LoginComponent",
   data() {
@@ -65,36 +67,36 @@ export default {
         },
       };
       axios
-        .post(Global.url + "login", this.contacto, config)
-        .then((response) => {
-          if (response.status == 200) {
-            let uncodeResponseData = JSON.parse(
-              window.atob(response.data.datos)
-            );
-            localStorage.setItem("auth_token", response.data.datos);
-            localStorage.setItem("auth_nombre", uncodeResponseData.nombre);
+          .post(Global.url + "login", this.contacto, config)
+          .then((response) => {
+            if (response.status == 200) {
+              let uncodeResponseData = JSON.parse(
+                  window.atob(response.data.datos)
+              );
+              localStorage.setItem("auth_token", response.data.datos);
+              localStorage.setItem("auth_nombre", uncodeResponseData.nombre);
 
-            if (uncodeResponseData.ou != "Bedelias") {
-              this.flashMessage.show({
-                status: "error",
-                title: Global.nombreSitio,
-                message: "Error, credenciales invalidas",
-              });
-              localStorage.clear();
-            } else {
-              this.$router.push("/home");
-              location.reload();
+              if (uncodeResponseData.ou != "Bedelias") {
+                this.flashMessage.show({
+                  status: "error",
+                  title: Global.nombreSitio,
+                  message: "Error, credenciales invalidas",
+                });
+                localStorage.clear();
+              } else {
+                this.$router.push("/home");
+                location.reload();
+              }
             }
-          }
-        })
-        .catch(() => {
-          this.flashMessage.show({
-            status: "error",
-            title: Global.nombreSitio,
-            message: "Error, credenciales invalidas",
+          })
+          .catch(() => {
+            this.flashMessage.show({
+              status: "error",
+              title: Global.nombreSitio,
+              message: "Error, credenciales invalidas",
+            });
+            document.form.reset();
           });
-          document.form.reset();
-        });
     },
   },
 };
