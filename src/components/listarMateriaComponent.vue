90cfa6bc61da622b209cb7addbad3ec6bb069b0c
aguskuster <61338886+aguskuster@users.noreply.github.com>
2022-07-25 16:08:21 -0300

    cambio tablas por tabla facha

--- src/components/listarMateriaComponent.vue
+++ src/components/listarMateriaComponent.vue
@@ -9,8 +9,7 @@
           data-bs-toggle="modal"
           data-bs-target="#modalAgregarMateria"
         >
-          <i class="far fa-books-medical"></i>
-          Materia
+          Agregar Materia
         </button>
       </div>
     </div>
@@ -18,80 +17,16 @@
     <div class="contenedorGeneral">
       <div class="contenedorIzquierdo">
         <h4>Asignaturas</h4>
-        <div class="menu_buscar" style="justify-content: center">
-          <input placeholder="Buscar..." />
-        </div>
-        <div class="contenedor_table">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Asignatura</th>
-                <th scope="col">&nbsp;</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="todo in todosMateria" :key="todo.id">
-                <td v-if="todo.id == idMateria && modificar">
-                  <input
-                    type="text"
-                    class="inputFachero"
-                    v-model="nuevoNombreMateria"
-                  />
-                </td>
-                <td v-else>{{ todo.nombre }}</td>
-                <td class="tdMateria">
-                  <i
-                    class="fas fa-times"
-                    style="
-                      font-size: 20px;
-                      margin-right: 5px;
-                      color: red;
-                      cursor: pointer;
-                      line-height: unset;
-                    "
-                    v-if="modificar && idMateria == todo.id"
-                    @click="cancelarModificar()"
-                  ></i>
 
-                  <i
-                    v-else
-                    v-on:click="alternarModificar(todo.id, true)"
-                    class="far fa-pen"
-                    style="
-                      font-size: 20px;
-                      margin-left: 19px;
-                      color: orange;
-                      line-height: unset;
-                    "
-                  ></i>
-                  <i
-                    class="fas fa-check"
-                    v-if="modificar && idMateria == todo.id"
-                    style="
-                      font-size: 20px;
-                      margin-right: 20px;
-                      color: green;
-                      cursor: pointer;
-                      line-height: unset;
-                    "
-                    v-on:click="modificarMateria(todo.id)"
-                  ></i>
-                  <i
-                    v-else
-                    class="fas fa-search"
-                    style="
-                      font-size: 20px;
-                      margin-left: 20px;
-                      color: blue;
-                      cursor: pointer;
-                      line-height: unset;
-                    "
-                    v-on:click="traerProfesoresMateria(todo.id, todo.nombre)"
-                  ></i>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div class="contenedor_table">
+          <vue-good-table
+            @on-row-click="onRowClick"
+            :columns="materiasColumnas"
+            :rows="todosMateria"
+            :search-options="{ enabled: true }"
+            theme="polar-bear"
+          >
+          </vue-good-table>
         </div>
       </div>
 
@@ -149,7 +84,6 @@
       <div class="contenedorDerechoPersona">
         <h4>Docentes que dictan {{ materiaSeleccionada }}</h4>
         <div class="menu_buscar">
-          <input placeholder="Buscar..." />
           <div class="botonesMateria">
             <button type="button" class="btn btn-success ml-2">
               <router-link
@@ -163,47 +97,44 @@
                   name: 'profesorMateria',
                   params: {
                     idMateria: idMateria,
-                    Materia: this.materiaSeleccionada,
+                    Materia: materiaSeleccionada,
                   },
                 }"
               >
                 <i class="fas fa-plus"></i>
               </router-link>
             </button>
-            <button type="button" class="btn btn-primary ml-2">
-              <i class="fas fa-pen" v-on:click="alternarEliminar()"></i>
-            </button>
             <button type="button" class="btn btn-danger ml-2">
-              <i class="fas fa-trash" v-on:click="eliminarMateria()"></i>
+              <i class="fas fa-trash" @click="sacarProfesor= !sacarProfesor"></i>
+            </button>
+            <button type="button" class="btn btn-primary ml-2">
+              <i class="fas fa-cog"></i><!-- Lo manda a otra pestaña y ahi le habilta cambiar nombre del materia o borrarla -->
             </button>
           </div>
         </div>
 
         <div class="contenedor_table">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Docente</th>
-                <th scope="col">Email</th>
-                <th scope="col">&nbsp;</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="todo in materiaProfesores" :key="todo.id">
-                <td>{{ todo.nombre }}</td>
-                <td>{{ todo.email }}</td>
-
-                <td v-if="sacarProfesor">
-                  <i
-                    class="fas fa-times"
-                    style="color: red; font-size: 16px"
-                    v-on:click="sacarProfesorMateria(todo)"
-                  ></i>
-                </td>
-                <td v-else><a href="">Ver Perfil</a></td>
-              </tr>
-            </tbody>
-          </table>
+          <vue-good-table
+            @on-selected-rows-change="selectionChanged"
+            :columns="profesorColumnas"
+            :rows="materiaProfesores"
+            :search-options="{ enabled: true }"
+            theme="polar-bear"
+            :select-options="{
+              enabled: true,
+              selectOnCheckboxOnly: true, // only select when checkbox is clicked instead of the row
+              selectionInfoClass: 'custom-class',
+              selectionText: 'rows selected',
+              clearSelectionText: 'clear',
+              disableSelectInfo: true, // disable the select info panel on top
+              selectAllByGroup: true, // when used in combination with a grouped table, add a checkbox in the header row to check/uncheck the entire group
+            }"
+          >
+          </vue-good-table>
+          <button v-if="sacarProfesor" class="btn-danger btn">
+            Eliminar Profesores
+          </button>
+          <!--  "Sweet alert listando nombres de los profes y confirmar" -->
         </div>
       </div>
     </div>
@@ -213,12 +144,14 @@
 import { Global } from "../Global";
 import axios from "axios";
 import vueHeadful from "vue-headful";
-
+import "vue-good-table/dist/vue-good-table.css";
+import { VueGoodTable } from "vue-good-table";
 export default {
   name: "listadoMaterias",
 
   components: {
     vueHeadful,
+    VueGoodTable,
   },
   data() {
     return {
@@ -226,7 +159,7 @@ export default {
       nav: true,
       todosMateria: "",
       materiaProfesores: "",
-      materiaSeleccionada: "",
+      materiaSeleccionada: " ",
       modificar: false,
       nuevoNombreMateria: "",
       idMateria: 0,
@@ -234,6 +167,23 @@ export default {
         nombreMateria: "",
       },
       sacarProfesor: false,
+      selectedRows: "",
+      materiasColumnas: [
+        {
+          label: "Asignatura",
+          field: "nombre",
+        },
+      ],
+      profesorColumnas: [
+        {
+          label: "Docente",
+          field: "nombre",
+        },
+        {
+          label: "Email",
+          field: "email",
+        },
+      ],
     };
   },
   mounted() {
@@ -272,11 +222,7 @@ export default {
           });
         });
     },
-    alternarEliminar() {
-      this.sacarProfesor
-        ? (this.sacarProfesor = false)
-        : (this.sacarProfesor = true);
-    },
+
     eliminarMateria() {
       axios
         .delete(Global.url + "materia", {
@@ -290,7 +236,6 @@ export default {
         })
         .then((response) => {
           if (response.status == 200) {
-          
             this.getTodos();
             this.flashMessage.show({
               status: "success",
@@ -307,6 +252,9 @@ export default {
           });
         });
     },
+    selectionChanged(params) {
+      this.selectedRows = params.selectedRows;
+    },
     alternarModificar(idMateria, boolean) {
       this.idMateria = idMateria;
       this.modificar = boolean;
@@ -322,7 +270,7 @@ export default {
         .post(Global.url + "materia", this.materia, config)
         .then((response) => {
           if (response.status == 200) {
-              console.log(response.data);
+            console.log(response.data);
             this.flashMessage.show({
               status: "success",
               title: Global.nombreSitio,
@@ -397,6 +345,10 @@ export default {
           });
         });
     },
+    onRowClick(params) {
+      this.traerProfesoresMateria(params.row.id, params.row.nombre);
+    },
+
     traerProfesoresMateria(idMateria, nombreMateria) {
       this.idMateria = idMateria;
       this.materiaSeleccionada = nombreMateria;
