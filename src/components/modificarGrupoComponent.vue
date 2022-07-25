bf1622858c13e79883ef5e8cdb48a7c7052dba7d
aguskuster <61338886+aguskuster@users.noreply.github.com>
2022-07-25 16:07:44 -0300

    modificar Grupo diseño

--- src/components/modificarGrupoComponent.vue
+++ src/components/modificarGrupoComponent.vue
@@ -1,130 +1,127 @@
 <template>
   <div>
     <div class="contenedor_menu">
- 
       <h2>Modificar Grupo</h2>
     </div>
-    <div
-      class="container p-3 my-3 border"
-      style="background-color: whitesmoke; height: 47rem"
-    >
-      <p>
-        <label for="nombreGrupo">Identificador del Grupo </label>
-        <input
-          type="text"
-          id="nombreGrupo"
-          name="nombreGrupo"
-          :value="idGrupo"
-        />
-      </p>
-      <p>
-        <label for="nombreGrupo">Descripcion del Grupo</label>
-        <input
-          type="text"
-          id="nombreGrupo"
-          name="nombreGrupo"
-          :value="grupoSeleccionado.nombreCompleto"
-        />
-      </p>
+    <div class="contenedorGeneral" style="justify-content: space-evenly">
+      <div
+        class="contenedorIzquierdo"
+        style="width: 30% !important; height: 47rem"
+      >
+        <h4>Grupo {{ idGrupo }}</h4>
+        <div class="m-2 p-2">
+          <label for="idGrupo" class="form-label">
+            Identificador de grupo</label
+          >
+          <input
+            type="text"
+            id="idGrupo"
+            disabled
+            class="form-control"
+            v-model="grupoSeleccionado.idGrupo"
+            @keyup="modificar = true"
+          />
+        </div>
+        <div class="m-2 p-2">
+          <label for="idGrupo" class="form-label">
+            Nombre completo de grupo</label
+          >
+          <input
+            type="text"
+            id="idGrupo"
+            class="form-control"
+            v-model="grupoSeleccionado.nombreCompleto"
+            @keyup="modificar = true"
+          />
+        </div>
 
-      <div class="ml-auto">
-        <button class="btn btn-secondary">Agregar Profesor</button>
-        <button class="btn btn-secondary">Agregar Alumnos</button>
+        <div class="" v-if="modificar">
+          <button class="btn btn-success mr-" @click="modificarGrupo()">
+            Actualizar
+          </button>
+          <button class="btn btn-danger" @click="getGrupo()">Cancelar</button>
+        </div>
+        <div v-else>
+          <button class="btn btn-danger" @click="eliminarGrupo()">
+            Eliminar Grupo
+          </button>
+        </div>
       </div>
 
-      <details>
-        <summary>Agregar Profesores</summary>
-
-        <div class="contTableProfesor">
-          <div>
-            <h3>Listado de Profesores</h3>
-            <hr />
-            <vue-good-table
-              @on-selected-rows-change="selectionChanged"
-              :columns="columns"
-              :rows="profesores"
-              :select-options="{
-                enabled: true,
-                selectOnCheckboxOnly: true, // only select when checkbox is clicked instead of the row
-                selectionInfoClass: 'custom-class',
-                selectionText: 'rows selected',
-                clearSelectionText: 'clear',
-                disableSelectInfo: true, // disable the select info panel on top
-                selectAllByGroup: true, // when used in combination with a grouped table, add a checkbox in the header row to check/uncheck the entire group
-              }"
-              :search-options="{ enabled: true }"
-              theme="polar-bear"
-            >
-            </vue-good-table>
+      <div
+        class="contenedorDerechoPersona"
+        style="width: 65% !important; height: 47rem"
+      >
+        <h4>Administrar Integrantes</h4>
+        <div style="width: 100%">
+          <button class="btn btn-primary">Agregar Miebros</button>
+        </div>
+        <div class="row p-2">
+          <div style="width: 45%" class="col">
+            <center>
+              <h5>Profesores</h5>
+            </center>
+            <ul class="list-group">
+              <li
+               v-for="todo in integrantesGrupo.profesores"
+                :key="todo.id"
+                class="
+                  list-group-item
+                  d-flex
+                  justify-content-between
+                  align-items-center
+                "
+              >
+               <img
+                  :src="b64Decode(todo.imagen_perfil)"
+                  width="50px"
+                  height="50px"
+                  style="border-radius: 50%;"
+                />
+                {{ todo.nombreProfesor }}
+                <span class="badge rounded-pill"
+                  ><i
+                    class="fas fa-times"
+                    style="color: red; font-size: 1rem"
+                  ></i
+                ></span>
+              </li>
+         
+            </ul>
           </div>
-          <div class="talbeProfesorDerecha">
-            <div>
-              <h3>Profesores Seleccionados</h3>
-              <hr />
-              <div class="contProfeSelec">
-                <div
-                  v-for="todo in selectedRows"
-                  :key="todo.id"
-                  data-toggle="tooltip"
-                  data-placement="top"
-                  :title="todo.nombre"
-                  style="cursor: help"
-                >
-                  {{ todo.nombre }}
-                </div>
-              </div>
-            </div>
-            <button class="btn-primary">Agregar Profesor</button>
+          <div style="width: 45%" class="col">
+            <center>
+              <h5>Alumnos</h5>
+            </center>
+            <ul class="list-group">
+              <li
+                v-for="todo in integrantesGrupo.alumnos"
+                :key="todo.id"
+                class="
+                  list-group-item
+                  d-flex
+                  justify-content-between
+                  align-items-center
+                "
+              >
+                <img
+                  :src="b64Decode(todo.imagen_perfil)"
+                  width="50px"
+                  height="50px"
+                  style="border-radius: 50%;"
+                />
+                {{ todo.nombreAlumno }}
+                <span class="badge rounded-pill"
+                  ><i
+                    class="fas fa-times"
+                    style="color: red; font-size: 1rem"
+                  ></i
+                ></span>
+              </li>
+            </ul>
           </div>
         </div>
-      </details>
-      <details>
-        <summary>Agregar Alumnos</summary>
-        <div class="contTableProfesor">
-          <div>
-            <h3>Listado de Alumnos</h3>
-            <hr />
-            <vue-good-table
-              @on-selected-rows-change="selectionChanged"
-              :columns="columns"
-              :rows="alumnos"
-              :select-options="{
-                enabled: true,
-                selectOnCheckboxOnly: true, // only select when checkbox is clicked instead of the row
-                selectionInfoClass: 'custom-class',
-                selectionText: 'rows selected',
-                clearSelectionText: 'clear',
-                disableSelectInfo: true, // disable the select info panel on top
-                selectAllByGroup: true, // when used in combination with a grouped table, add a checkbox in the header row to check/uncheck the entire group
-              }"
-              :search-options="{ enabled: true }"
-              theme="polar-bear"
-            >
-            </vue-good-table>
-          </div>
-          <div class="talbeProfesorDerecha">
-            <div>
-              <h3>Alumnos Seleccionados</h3>
-              <hr />
-              <div class="contProfeSelec">
-                <div
-                  v-for="todo in selectedRows"
-                  :key="todo.id"
-                  data-toggle="tooltip"
-                  data-placement="top"
-                  :title="todo.nombre"
-                  style="cursor: help"
-                >
-                  {{ todo.nombre }}
-                </div>
-              </div>
-            </div>
-            <button class="btn-primary">Agregar Alumnos</button>
-          </div>
-        </div>
-      </details>
-       <button class="btn-danger btn">Cancelar</button>
-      <button class="btn-success btn">Guardar Cambios</button>
+      </div>
     </div>
   </div>
 </template>
@@ -134,17 +131,19 @@
 <script>
 import { Global } from "../Global";
 import axios from "axios";
-import "vue-good-table/dist/vue-good-table.css";
-import { VueGoodTable } from "vue-good-table";
+
+/* import "vue-good-table/dist/vue-good-table.css";
+import { VueGoodTable } from "vue-good-table"; */
 export default {
   name: "modificarUsuarioComponent.vue",
   components: {
-    VueGoodTable,
+    /*    VueGoodTable, */
   },
   data() {
     return {
       idGrupo: this.$route.params.idGrupo,
       modificar: false,
+      integrantesGrupo: { profesores: {}, alumnos: {} },
       grupoSeleccionado: "",
       columns: [
         {
@@ -163,12 +162,39 @@ export default {
   },
   mounted() {
     this.getGrupo();
+    this.buscarGrupoSeleccionado();
   },
   methods: {
+    b64Decode(img) {
+      return "data:image/png;base64," + img;
+    },
+    buscarGrupoSeleccionado() {
+      let config = {
+        headers: {
+          "Content-Type": "application/json",
+          token: Global.token,
+        },
+      };
+      axios
+        .get(Global.url + "integrantes-curso?idGrupo=" + this.idGrupo, config)
+        .then((res) => {
+          if (res.status == 200) {
+            this.integrantesGrupo = res.data;
+          }
+        })
+        .catch(() => {
+          this.flashMessage.show({
+            status: "warning",
+            title: Global.nombreSitio,
+            message: "Error inesperado al cargar",
+          });
+        });
+    },
     selectionChanged(params) {
       this.selectedRows = params.selectedRows;
     },
     getGrupo() {
+      this.modificar = false;
       let config = {
         headers: {
           token: Global.token,
@@ -183,12 +209,42 @@ export default {
           }
         });
     },
+    eliminarGrupo() {
+      /*      axios
+        .delete(Global.url + "grupo", {
+          headers: {
+            "Content-Type": "application/json",
+            token: Global.token,
+          },
+          data: {
+            idGrupo: this.GrupoDatos.idGrupo,
+          },
+        })
+        .then((response) => {
+          if (response.status == 200) {
+            this.flashMessage.show({
+              status: "success",
+              title: Global.nombreSitio,
+              message: "Grupo Eliminado",
+            });
+            this.$router.push("/listarGrupo");
+          }
+        })
+        .catch(() => {
+          this.flashMessage.show({
+            status: "error",
+            title: Global.nombreSitio,
+            message: "Error Inesperado",
+          });
+        }); */
+      alert("Sweet alert confirmacion");
+    },
+
     modificarGrupo() {
       let parametros = {
         idGrupo: this.idGrupo,
-        nuevoGrupo: document.getElementById("nombreAcronimo").value,
-        nuevoNombreCompleto: document.getElementById("nombreCompletoGrupo")
-          .value,
+        nuevoGrupo: this.grupoSeleccionado.idGrupo,
+        nuevoNombreCompleto: this.grupoSeleccionado.nombreCompleto,
       };
       let config = {
         headers: {
