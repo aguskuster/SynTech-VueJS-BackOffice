be42d1f84a86d55c0d050d5d9fb4d8f9f273d545
aguskuster <61338886+aguskuster@users.noreply.github.com>
2022-08-14 16:38:05 -0300

    Actualizar Usuario  Info

--- src/components/modificarUsuarioComponent.vue
+++ src/components/modificarUsuarioComponent.vue
@@ -11,7 +11,7 @@
       >
         <div class="imgModificarUser">
           <center>
-            <img :src="returnImgProfile(usuarioDatos.imagen_perfil)" alt="" />
+            <img :src="usuarioDatos.imagen_perfil" alt="" />
 
             <h3>{{ usuarioDatos.nombre }}</h3>
             <hr />
@@ -19,14 +19,20 @@
           <p class="text-muted">CI: {{ usuarioDatos.id }}</p>
         </div>
         <div>
-          <div style="position: absolute; bottom: 10px; left: 37px; ">
-            <button class="btn btn-primary"  style='min-width:220px' @click="comprobarAccion('foto')">
+          <div style="position: absolute; bottom: 10px; left: 37px">
+            <button
+              class="btn btn-primary"
+              style="min-width: 220px"
+              @click="comprobarAccion('foto')"
+              v-if="usuario.cargo != 'Adscripto'"
+            >
               Restablecer Foto
             </button>
             <button
               class="btn btn-primary"
-              style="margin-left:55px;min-width:220px"
+              style="margin-left: 55px; min-width: 220px"
               @click="comprobarAccion('contraseña')"
+              v-if="usuario.cargo != 'Adscripto'"
             >
               Restablecer Contraseña
             </button>
@@ -41,11 +47,11 @@
         <div class="formModificar">
           <div class="informacion-izquierda">
             <h3 style="text-transform: uppercase">Informacion Personal</h3>
-            <div class="personalDetails">
+            <div class="personalDetails" v-if="usuario.cargo != 'Adscripto'">
               <div class="mb-3">
                 <p style="font-size: 18px">Nombre</p>
                 <input
-                  :value="nombre"
+                  v-model="nombre"
                   class="form-control inputFachero"
                   style="height: 50px; font-size: 16px"
                 />
@@ -53,7 +59,7 @@
               <div class="mb-3">
                 <p style="font-size: 18px">Apellido</p>
                 <input
-                  :value="apellido"
+                  v-model="apellido"
                   class="form-control inputFachero"
                   style="height: 50px; font-size: 16px"
                 />
@@ -61,7 +67,7 @@
               <div class="mb-3">
                 <p style="font-size: 18px">Mail</p>
                 <input
-                  :value="usuarioDatos.email"
+                  v-model="usuarioDatos.email"
                   class="form-control inputFachero"
                   style="height: 50px; font-size: 16px"
                 />
@@ -69,7 +75,8 @@
               <div class="mb-3">
                 <p style="font-size: 18px">Rol</p>
                 <input
-                  :value="usuarioDatos.ou"
+                  disabled
+                  v-model="usuarioDatos.ou"
                   class="form-control inputFachero"
                   style="height: 50px; font-size: 16px"
                 />
@@ -77,7 +84,55 @@
               <div class="mb-3">
                 <p style="font-size: 18px">Genero</p>
                 <input
-                  :value="usuarioDatos.genero"
+                  v-model="usuarioDatos.genero"
+                  class="form-control inputFachero"
+                  style="height: 50px; font-size: 16px"
+                />
+              </div>
+            </div>
+
+            <div class="personalDetails" v-else>
+              <div class="mb-3">
+                <p style="font-size: 18px">Nombre</p>
+                <input
+                  v-model="nombre"
+                  disabled
+                  class="form-control inputFachero"
+                  style="height: 50px; font-size: 16px"
+                />
+              </div>
+              <div class="mb-3">
+                <p style="font-size: 18px">Apellido</p>
+                <input
+                  v-model="apellido"
+                  disabled
+                  class="form-control inputFachero"
+                  style="height: 50px; font-size: 16px"
+                />
+              </div>
+              <div class="mb-3">
+                <p style="font-size: 18px">Mail</p>
+                <input
+                  v-model="usuarioDatos.email"
+                  disabled
+                  class="form-control inputFachero"
+                  style="height: 50px; font-size: 16px"
+                />
+              </div>
+              <div class="mb-3">
+                <p style="font-size: 18px">Rol</p>
+                <input
+                  disabled
+                  v-model="usuarioDatos.ou"
+                  class="form-control inputFachero"
+                  style="height: 50px; font-size: 16px"
+                />
+              </div>
+              <div class="mb-3">
+                <p style="font-size: 18px">Genero</p>
+                <input
+                  disabled
+                  v-model="usuarioDatos.genero"
                   class="form-control inputFachero"
                   style="height: 50px; font-size: 16px"
                 />
@@ -110,11 +165,20 @@
               </div>
             </div>
           </div>
-          <div style="position: absolute; right: 40px; bottom:10px">
-            <button class="btn btn-success" style="margin-right: 10px">
+          <div style="position: absolute; right: 40px; bottom: 10px">
+            <button
+              class="btn btn-success"
+              style="margin-right: 10px"
+              @click="comprobarModificarInfo()"
+              v-if="usuario.cargo != 'Adscripto'"
+            >
               Actualizar
             </button>
-            <button class="btn btn-danger" v-on:click="$router.back()">
+            <button
+              class="btn btn-danger"
+              v-on:click="$router.back()"
+              v-if="usuario.cargo != 'Adscripto'"
+            >
               Cancelar
             </button>
           </div>
@@ -141,6 +205,56 @@ export default {
     this.getUsuario();
   },
   methods: {
+    comprobarModificarInfo() {
+      this.$swal
+        .fire({
+          icon: "info",
+          title: "Modificar Usuario",
+          html:
+            "¿ Estas seguro de que desea modificar la informacion del usuario <u><b>" +
+            this.usuarioDatos.nombre +
+            " </b></u> ?",
+          showCancelButton: true,
+          cancelButtonText: "Cancelar",
+          confirmButtonText: "Modificar",
+        })
+        .then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            alert("Paso iF");
+            this.modificarUsuario();
+          } else {
+            alert("Paso Flase");
+            return false;
+          }
+        });
+    },
+    modificarUsuario() {
+      let config = {
+        headers: {
+          token: Global.token,
+        },
+      };
+      let user = {
+        idUsuario: this.usuarioDatos.id,
+        nombre: this.nombre + " " + this.apellido,
+        email: this.usuarioDatos.email,
+        genero: this.usuarioDatos.genero,
+      };
+
+      console.log(user);
+      axios
+        .put(Global.url + "usuario", user, config)
+        .then((res) => {
+          if (res.status == 200) {
+            this.$swal.fire("Usuario Modificado", "", "success");
+          }
+          this.getUsuario();
+        })
+        .catch(() => {
+          this.$swal.fire("Error al modifcar usuario", "", "error");
+        });
+    },
     comprobarAccion(accion) {
       this.$swal
         .fire({
@@ -174,7 +288,7 @@ export default {
       };
       let user = { id: this.$route.params.user };
       axios
-        .put(Global.url + "foto?", user, config)
+        .put(Global.url + "foto", user, config)
         .then((res) => {
           if (res.status == 200) {
             this.$swal.fire("Foto actualizada", "", "success");
@@ -217,6 +331,9 @@ export default {
             this.usuarioDatos = res.data.user;
             this.usuarioInfo = res.data.info;
             this.recortarNombre();
+            this.usuarioDatos.imagen_perfil = this.returnImgProfile(
+              this.usuarioDatos.imagen_perfil
+            );
           }
         })
         .catch(() => {
