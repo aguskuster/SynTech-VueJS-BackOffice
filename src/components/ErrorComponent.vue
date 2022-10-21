adefd59a6e2caf625014b403ac78ec6efbb24e8d
Agustin Kuster <61338886+aguskuster@users.noreply.github.com>
2022-10-21 18:11:59 -0300

    fix page 404

--- src/components/ErrorComponent.vue
+++ src/components/ErrorComponent.vue
@@ -1,24 +1,23 @@
 <template>
-  <div>
-    <h1>Página no encontrada</h1>
+  <div style="background-color:whitesmoke;height:2000px;margin-top:-70px">
+ 
     <vue-headful :title="title" />
     <br />
     <center>
-      <div style="display: flex; flex-direction: row;justify-content:center">
-        <div style="width: 10%">
+        <br><br><br>
+        
           <img
-          width="200px"
-            src="https://i.pinimg.com/originals/76/ba/75/76ba7583218cf74f05fd5607b7d1a709.png"
+            width="300px"
+            src="../assets/images/unknow_page.png"
             alt=""
           />
-        </div>
+          <br>
+             <h1 style="font-size:40px;margin-top:30px">ERROR 404</h1>
       
-        <h5 style="width: 20%;margin-top:100px">
-          Oops, parece que te perdistes por el sendero de la vida . . .
-        </h5>
-      </div>
+        
+     
 
-      <small><a href="/home">Volver Inicio</a></small>
+      
     </center>
   </div>
 </template>
