24d7cffab2181219e159a789543f0e4d1e87fa16
Agustin Kuster <61338886+aguskuster@users.noreply.github.com>
2023-04-10 22:08:01 -0300

    Welcome page

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    <h1>Página Home</h1>
-    <div class="container p-3 my-3 border">
-      <vue-headful :title="title" />
+<div class="d-flex align-items-center justify-content-center" style="height: 60vh;">
+  <vue-headful :title="title" />
+  <div class="d-flex">
+   
+    <div class="text-center">
+      <h2 style="font-size:45px;font-weight:600;"> ¡ Bienvenido !</h2>
+      <br>
+      <p >Usa la barra navegadora de la izquierda para crear, modificar, o eliminar contenido.</p>
       
     </div>
   </div>
+  <div class="position-absolute bottom-0 end-0">
+    <img src="../assets/images/hello.svg" alt="Imagen de una persona saludando" width="750" height="350">
+  </div>
+</div>
 </template>
 
 <script>
