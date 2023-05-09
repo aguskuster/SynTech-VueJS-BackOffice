943d5ea4370e6d084677fc757152986ed16cfb96
Federico Blengio <fedeeblengio@gmail.com>
2023-05-08 23:36:21 -0300

    bed

--- src/Global.js
+++ src/Global.js
@@ -1,6 +1,6 @@
 export var Global = {
     // url: 'http://190.64.41.140:8000/api/',
-    url: 'http://192.168.50.137:8000/api/',
+    url: 'http://190.64.41.140:8000/api/',
     token: localStorage.getItem("auth_token_BO"),
     nombreSitio: 'BackOffice',
     spinnerUrl: 'https://www.sanandreasregional.org/app/plugins/unyson/framework/extensions/forms/static/img/ajax-loader.gif',
