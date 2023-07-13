ddaebb9bf123459686f2405396a3d1fb524701b0
Agustin Kuster <61338886+aguskuster@users.noreply.github.com>
2023-07-12 22:52:05 -0300

    change ip

--- src/Global.js
+++ src/Global.js
@@ -1,6 +1,7 @@
 export var Global = {
     // url: 'http://190.64.41.140:8000/api/',
-    url: 'http://192.168.50.137:8000/api/',
+    // url: 'http://192.168.50.137:8000/api/',
+    url: 'http://192.168.50.150:8000/api',
     token: localStorage.getItem("auth_token_BO"),
     nombreSitio: 'BackOffice',
     spinnerUrl: 'https://www.sanandreasregional.org/app/plugins/unyson/framework/extensions/forms/static/img/ajax-loader.gif',
