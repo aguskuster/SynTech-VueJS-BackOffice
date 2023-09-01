746d898b23defa906150474b42476ce152d744c4
Agustin Kuster <61338886+aguskuster@users.noreply.github.com>
2023-09-01 20:38:20 -0300

    Update Global.js

--- src/Global.js
+++ src/Global.js
@@ -1,10 +1,10 @@
 export var Global = {
-    url: 'http://10.106.5.150:8000/api/',
+    url: 'http://192.168.200.150:8000/api/',
     token: localStorage.getItem("auth_token_BO"),
     nombreSitio: 'BackOffice',
     spinnerUrl: 'https://www.sanandreasregional.org/app/plugins/unyson/framework/extensions/forms/static/img/ajax-loader.gif',
-    matomoSitio : "http://10.106.5.151/index.php?module=CoreHome&action=index&date=yesterday&period=day&idSite=3#?period=day&date=yesterday&category=Dashboard_Dashboard&subcategory=1",
-    matomoBackoffice: "http://10.10.5.151/index.php?module=CoreHome&action=index&date=yesterday&period=day&idSite=2#?period=day&date=yesterday&category=Dashboard_Dashboard&subcategory=1"
+    matomoSitio : "http://192.168.200.151/index.php?module=CoreHome&action=index&date=yesterday&period=day&idSite=3#?period=day&date=yesterday&category=Dashboard_Dashboard&subcategory=1",
+    matomoBackoffice: "http://192.168.200.151/index.php?module=CoreHome&action=index&date=yesterday&period=day&idSite=2#?period=day&date=yesterday&category=Dashboard_Dashboard&subcategory=1"
 }
 
 export var roles = {
